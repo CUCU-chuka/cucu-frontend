80c2e75f8c8060fc3febe01cdc563cbf065ea554
Rakyrish <mbuguajohn367@gmail.com>
2026-03-10 10:17:28 +0300

    creating assets

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
+
+  server: {
+    proxy: {
+      
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+
+      
+      '/media': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
